6c18b87d158bc08b8cd2212a14ddab441d6c3a93
Maxim <creativesun@yandex.ru>
2021-12-26 14:58:19 +0300

    123

--- 12/12-2.cpp
+++ 12/12-2.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 
-int square(int number)
-{
+int square(int number) {
     return number * number;
 }
 
-int main()
-{
+int main() {
     // 2. Функция, возвращающая квадрат числа (степень 2).
     std::cout << square(7);
     return 0;
